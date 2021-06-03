d830d5fac3cbc58d125536b80f967fa2a21372b3
Jan Löwenstrom <jan.loewenstrom@web.de>
2021-06-03 14:23:55 +0200

    load and try predictions on custom images

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -1,12 +1,14 @@
 import torchvision
 import torchvision.transforms as transforms
+from pathlib import Path
 
 transform = transforms.Compose([transforms.ToTensor()])
 
 # Load train and test datasets
+path = (Path(__file__).parents[2] / "data/").resolve()
 trainset = torchvision.datasets.MNIST(
-    "./data/", train=True, download=True, transform=transform
+    path, train=True, download=True, transform=transform
 )
 testset = torchvision.datasets.MNIST(
-    "./data", train=False, download=True, transform=transform
+    path, train=False, download=True, transform=transform
 )
